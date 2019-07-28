9622b8daa73620940f32bb043e0c698facaf2129
vanderkilu <vanderkiliku@gmail.com>
2019-07-28 20:12:55 +0000

    add authorization

---
+++ server/auth/index.js
@@ -0,0 +1,31 @@
+const jwt = require('express-jwt');
+const secret = require('../config').secret;
+
+// this is used to authorize users
+// required means token should be provided
+// optional means token is optional
+
+function getTokenFromHeader(req){
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token' ||
+      req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    return req.headers.authorization.split(' ')[1];
+  }
+
+  return null;
+}
+
+const auth = {
+  required: jwt({
+    secret: secret,
+    userProperty: 'payload',
+    getToken: getTokenFromHeader
+  }),
+  optional: jwt({
+    secret: secret,
+    userProperty: 'payload',
+    credentialsRequired: false,
+    getToken: getTokenFromHeader
+  })
+};
+
+module.exports = auth;
