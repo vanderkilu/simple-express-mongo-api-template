6c8bc2c0bc8b662bb5b41baa363a81d5e99432da
vanderkilu <vanderkiliku@gmail.com>
2019-07-31 16:23:57 +0000

    add ability to convert bufffer to data uri

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,5 +1,18 @@
 const multer = require('multer')
-const storage = multer.memoryStorage();
-const multerUploads = multer({ storage }).single('image');
+const dataURI = require('datauri')
+const path = require('path')
 
-module.exports = multerUploads
+const storage = multer.memoryStorage();
+const uploadMiddleWare = multer({ storage }).single('image');
+
+const storageUrl = new dataURI()
+const toStorageUrl = (req)=> {
+    storageUrl.format(path.extname(req.file.originalname).toString(),
+    req.file.buffer)
+}
+
+
+module.exports = {
+    uploadMiddleWare,
+    toStorageUrl
+}
