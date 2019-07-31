96a12af3ff40d814b283c6bce1f473d642356bbf
vanderkilu <vanderkiliku@gmail.com>
2019-07-31 16:10:10 +0000

    create config for cloudinary

---
+++ server/config/cloudinary.js
@@ -0,0 +1,11 @@
+const cloudinary = require('cloudinary').v2
+
+const cloudinaryConfig = (req, res, next)=> {
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    })
+    next()
+}
+module.exports = cloudinaryConfig
